5d4730e846bc69eee82316ce169145c28e163788
zhangbo1006 <744207346@qq.com>
2019-04-11 22:19:36 +0800

    0411 网络请求判断

--- app/src/main/java/com/wd/tech/mvp/model/utils/NetworkChangeReceiverUtil.kt
+++ app/src/main/java/com/wd/tech/mvp/model/utils/NetworkChangeReceiverUtil.kt
@@ -6,6 +6,7 @@ import android.content.Context.CONNECTIVITY_SERVICE
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.widget.LinearLayout
 import android.widget.Toast
 
 class NetworkChangeReceiverUtil : BroadcastReceiver() {
@@ -18,6 +19,7 @@ class NetworkChangeReceiverUtil : BroadcastReceiver() {
                     Toast.makeText(context, "正在使用移动网络", Toast.LENGTH_SHORT).show()
             }
         }else{
+            
             Toast.makeText(context, "当前无网络连接", Toast.LENGTH_SHORT).show()
         }
     }
