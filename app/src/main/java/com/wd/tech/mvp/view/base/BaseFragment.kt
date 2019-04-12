d850d706c93fd1362c3d1f3cdeb5de8b40b25083
zhangbo1006 <744207346@qq.com>
2019-04-12 15:19:50 +0800

    no message

--- app/src/main/java/com/wd/tech/mvp/view/base/BaseFragment.kt
+++ app/src/main/java/com/wd/tech/mvp/view/base/BaseFragment.kt
@@ -1,4 +1,36 @@
 package com.wd.tech.mvp.view.base
 
-class BaseFragment {
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wd.tech.mvp.presenter.base.BasePresenter
+
+abstract class BaseFragment<V,T : BasePresenter<V>> : Fragment() {
+
+    var mPresenter : BasePresenter<V> = BasePresenter()
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+        initFragmentView(inflater)
+        initFragmentChildView()
+        initFragmentData()
+    }
+
+    abstract protected fun initFragmentView(inflater : LayoutInflater) : View
+
+    abstract protected fun initFragmentChildView();
+
+    abstract protected fun initFragmentData();
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mPresenter.detachView()
+    }
 }
