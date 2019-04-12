0117158d8d0e5d9d77e5c128a50d8b5402daacbc
zhangbo1006 <744207346@qq.com>
2019-04-12 14:07:43 +0800

    0412 网络判断

--- app/src/main/java/com/wd/tech/mvp/model/utils/NetworkChangeReceiverUtil.kt
+++ app/src/main/java/com/wd/tech/mvp/model/utils/NetworkChangeReceiverUtil.kt
@@ -1,13 +1,17 @@
 package com.wd.tech.mvp.model.utils
 
+import android.app.AlertDialog
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Context.CONNECTIVITY_SERVICE
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
-import android.widget.LinearLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
+import com.wd.tech.R
 
 class NetworkChangeReceiverUtil : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -19,7 +23,12 @@ class NetworkChangeReceiverUtil : BroadcastReceiver() {
                     Toast.makeText(context, "正在使用移动网络", Toast.LENGTH_SHORT).show()
             }
         }else{
-            
+            var view : View = LayoutInflater.from(context).inflate(R.layout.network_change_dialog,null)
+            var bulider : AlertDialog.Builder = AlertDialog.Builder(context)
+            var alertDialog : AlertDialog = bulider.create()
+            alertDialog.window.setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT)
+            alertDialog.setView(view)
+            alertDialog.show()
             Toast.makeText(context, "当前无网络连接", Toast.LENGTH_SHORT).show()
         }
     }
