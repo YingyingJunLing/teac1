9e6fcf45402cde82b5bae4ea647bbde4dde35b24
zhangbo1006 <744207346@qq.com>
2019-04-14 20:36:57 +0800

    no message

--- app/src/main/java/com/wd/tech/mvp/view/base/BaseFragment.kt
+++ app/src/main/java/com/wd/tech/mvp/view/base/BaseFragment.kt
@@ -17,39 +17,35 @@ abstract class BaseFragment<V,T : BasePresenter<V>> : Fragment(), CustomAdapt {
     override fun getSizeInDp(): Float {
         return 667F
     }
-
     var mPresenter : BasePresenter<V> = BasePresenter()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    //加载视图
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = initFragmentView(inflater)
+        var view : View = initFragmentView(inflater)
+        initFragmentData(savedInstanceState)
         return view
     }
 
+    //加载子控件
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initFragmentChildView(view)
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if (savedInstanceState != null) {
-            initFragmentData(savedInstanceState)
-        }
+        initFragmentData(savedInstanceState)
     }
 
-    abstract fun initFragmentView(inflater : LayoutInflater): View
 
-    abstract fun initFragmentChildView(view : View)
+    abstract protected fun initFragmentView(inflater : LayoutInflater) : View
 
-    abstract fun initFragmentData(savedInstanceState : Bundle)
+    abstract protected fun initFragmentChildView(view : View);
+
+    abstract protected fun initFragmentData(savedInstanceState: Bundle?);
 
     override fun onDestroy() {
         super.onDestroy()
-        if (null != mPresenter) {
-            mPresenter.detachView()
-        }
+        mPresenter.detachView()
     }
 }
--- app/src/main/java/com/wd/tech/mvp/view/frag/CommunityFragment.kt
+++ app/src/main/java/com/wd/tech/mvp/view/frag/CommunityFragment.kt
@@ -1,23 +1,22 @@
 package com.wd.tech.mvp.view.frag
 
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.wd.tech.R
 import com.wd.tech.mvp.model.bean.CommunityListBean
-import com.wd.tech.mvp.model.bean.CommunityListBeanResult
 import com.wd.tech.mvp.presenter.presenterimpl.CommunityListPresenter
 import com.wd.tech.mvp.view.adapter.CommunityListAdapter
 import com.wd.tech.mvp.view.base.BaseFragment
 import com.wd.tech.mvp.view.contract.Contract
-import kotlinx.android.synthetic.main.frag_community.*
-import kotlinx.android.synthetic.main.frag_community.view.*
 
 class CommunityFragment : BaseFragment<Contract.ICommunityListView,CommunityListPresenter>(),Contract.ICommunityListView {
+
+    lateinit var adapter: CommunityListAdapter
+
     override fun initFragmentView(inflater: LayoutInflater): View {
-        var view : View = View.inflate(context,R.layout.frag_community,null)
+        val view : View = View.inflate(context,R.layout.frag_community,null)
         return view
     }
 
@@ -25,12 +24,17 @@ class CommunityFragment : BaseFragment<Contract.ICommunityListView,CommunityList
 
     }
 
-    override fun initFragmentData(savedInstanceState: Bundle) {
-
+    override fun initFragmentData(savedInstanceState: Bundle?) {
+        val communityListPresenter : CommunityListPresenter = CommunityListPresenter(this)
+        val hashMap : HashMap<String,Int> = HashMap()
+        communityListPresenter.onICommunityListPre(hashMap,1,10)
     }
 
     override fun onSuccess(communityListBean: CommunityListBean) {
-
+        val list = communityListBean.result
+        Log.i("列表",list.get(0).content)
+        /*adapter = CommunityListAdapter(this.requireContext(),list)
+        xRecycle_community.adapter = adapter*/
     }
 
     override fun onFail() {
