82e99434a392d2e4010571edf5ef5ec1d1efbed8
zhangbo1006 <744207346@qq.com>
2019-04-27 21:02:50 +0800

    no message

--- app/src/main/java/com/wd/tech/mvp/view/adapter/FriendMessageAdapter.kt
+++ app/src/main/java/com/wd/tech/mvp/view/adapter/FriendMessageAdapter.kt
@@ -2,13 +2,16 @@ package com.wd.tech.mvp.view.adapter
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import cn.jpush.im.android.api.model.Conversation
+import cn.jpush.im.android.api.JMessageClient
 import cn.jpush.im.android.api.model.Message
+import com.facebook.drawee.view.SimpleDraweeView
 import com.wd.tech.R
+import com.wd.tech.mvp.model.utils.FrescoUtil
 import kotlinx.android.synthetic.main.friend_message_item_layout.view.*
 
 class FriendMessageAdapter(context: Context, list: MutableList<Message>) : RecyclerView.Adapter<FriendMessageAdapter.MyViewHolder>(){
@@ -26,15 +29,33 @@ class FriendMessageAdapter(context: Context, list: MutableList<Message>) : Recyc
     }
 
     override fun onBindViewHolder(p0: MyViewHolder, p1: Int) {
-        p0.message_text.setText(list.get(p1).content.toJson())
+        val head_img = list.get(p1).fromUser.address
+        val content = list.get(p1).content.toJson().split("\"")
+        p0.message_text.setText(content.get(3))
+        if (JMessageClient.getMyInfo().userName == list.get(p1).fromUser.userName){
+            FrescoUtil.setPic(head_img,p0.mine_img)
+            p0.view_friend.visibility = View.GONE
+        }else{
+            FrescoUtil.setPic(head_img,p0.friend_img)
+            p0.view_mine.visibility = View.GONE
+        }
     }
 
     class MyViewHolder : RecyclerView.ViewHolder {
 
         var message_text : TextView
+        var friend_img : SimpleDraweeView
+        var mine_img : SimpleDraweeView
+        var view_friend : View
+        var view_mine : View
 
         constructor(view : View) : super(view){
             message_text = view.message_text
+            friend_img = view.friend_img
+            mine_img = view.mine_img
+            view_mine = view.view_mine
+            view_friend = view.view_friend
+
         }
     }
 }
